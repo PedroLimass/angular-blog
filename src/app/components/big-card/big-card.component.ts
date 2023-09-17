7353713fd166aa12ed3ac2f1d526bc12f740d013
Pedro Lima <phlimas@outlook.com>
2023-09-17 15:13:37 -0300

    feat: Added new components

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css']
+})
+export class BigCardComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() UrlContet:string = ''
+  ngOnInit(): void {
+  }
+
+}
